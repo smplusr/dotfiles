f09333081f79052c64b0474a52438810445b4208
smplusr <83000221+smplusr@users.noreply.github.com>
2021-09-10 20:15:53 +0200

    Update package_setup.sh

--- utilities/package_setup.sh
+++ utilities/package_setup.sh
@@ -5,7 +5,7 @@ read -p "[INFO] - Answer: Y - N | " -n 1 -r
 echo ""
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	apk add alpine-conf xf86-video-nouveau xf86-video-nv xf86-video-vesa xf86-video-fbdev bspwm sxhkd vim sudo neofetch htop acpi pm-utils picom polybar feh xrandr bluez git make gcc g++ glfw glfw-dev openal-soft openal-soft-dev libx11-dev libxft-dev libxinerama-dev ncurses dbus-x11 font-tewi ttf-font-awesome adwaita-gtk2-theme adwaita-icon-theme nerd-fonts alsa-utils alsa-lib alsaconf linux-headers
+	apk add alpine-conf xf86-video-nouveau xf86-video-nv xf86-video-vesa xf86-video-fbdev bspwm sxhkd vim sudo neofetch htop acpi pm-utils picom polybar feh xrandr bluez git make gcc g++ glfw glfw-dev openal-soft openal-soft-dev libx11-dev libxft-dev libxinerama-dev ncurses dbus-x11 x11vnc font-tewi ttf-font-awesome adwaita-gtk2-theme adwaita-icon-theme nerd-fonts alsa-utils alsa-lib alsaconf linux-headers
 else
 	echo ""
 	echo "[INFO] - Using offline package installing ?"
@@ -21,8 +21,8 @@ else
 		if [[ $REPLY =~ ^[Yy]$ ]]
 		then
 			cd ../packages
-			apk fetch --recursive --force-broken-world --force-old-apk --force-overwrite --force-refresh alpine-conf xf86-video-nouveau xf86-video-nv xf86-video-vesa xf86-video-fbdev bspwm sxhkd vim sudo neofetch htop acpi pm-utils picom polybar feh xrandr bluez git make gcc g++ glfw glfw-dev openal-soft openal-soft-dev libx11-dev libxft-dev libxinerama-dev ncurses dbus-x11 font-tewi ttf-font-awesome adwaita-gtk2-theme adwaita-icon-theme nerd-fonts alsa-utils alsa-lib alsaconf linux-headers
-			apk fetch alpine-conf xf86-video-nouveau xf86-video-nv xf86-video-vesa xf86-video-fbdev alpine-conf bspwm sxhkd vim sudo neofetch htop acpi pm-utils picom polybar feh xrandr bluez git make gcc g++ glfw glfw-dev openal-soft openal-soft-dev libx11-dev libxft-dev libxinerama-dev ncurses dbus-x11 font-tewi ttf-font-awesome adwaita-gtk2-theme adwaita-icon-theme nerd-fonts alsa-utils alsa-lib alsaconf linux-headers
+			apk fetch --recursive --force-broken-world --force-old-apk --force-overwrite --force-refresh alpine-conf xf86-video-nouveau xf86-video-nv xf86-video-vesa xf86-video-fbdev bspwm sxhkd vim sudo neofetch htop acpi pm-utils picom polybar feh xrandr bluez git make gcc g++ glfw glfw-dev openal-soft openal-soft-dev libx11-dev libxft-dev libxinerama-dev ncurses dbus-x11 x11vnc font-tewi ttf-font-awesome adwaita-gtk2-theme adwaita-icon-theme nerd-fonts alsa-utils alsa-lib alsaconf linux-headers
+			apk fetch alpine-conf xf86-video-nouveau xf86-video-nv xf86-video-vesa xf86-video-fbdev alpine-conf bspwm sxhkd vim sudo neofetch htop acpi pm-utils picom polybar feh xrandr bluez git make gcc g++ glfw glfw-dev openal-soft openal-soft-dev libx11-dev libxft-dev libxinerama-dev ncurses dbus-x11 x11vnc font-tewi ttf-font-awesome adwaita-gtk2-theme adwaita-icon-theme nerd-fonts alsa-utils alsa-lib alsaconf linux-headers
 			cd ../
 		fi
 		apk add --allow-untrusted ../packages/*.apk
